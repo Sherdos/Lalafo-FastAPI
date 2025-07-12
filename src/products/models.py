994b75837202c67d06e0a600176f40afe9ad672f
Sherdos <110164213+Sherdos@users.noreply.github.com>
2025-07-12 18:00:01 +0600

    feat: new products, category, add alembic

---
+++ src/products/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, ForeignKey, Column
+
+from src.models import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    """ id: int = Column(Integer, primary_key=True)"""
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    price: Mapped[float] = mapped_column(Integer, nullable=False)
+    image: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id"), nullable=False
+    )
